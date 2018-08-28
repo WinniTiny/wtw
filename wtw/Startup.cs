bb9e0b00b90b6c56dd777a330eda2258aa236473
jupiterleaf <pikaq.snail@gmail.com>
2018-08-29 01:22:21 +1000

    Add project files.

---
+++ wtw/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(wtw.Startup))]
+namespace wtw
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
